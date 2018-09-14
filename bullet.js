f0de94608b4e089d4738864f13aee11daa6313dd
edSBHS <41878755+edSBHS@users.noreply.github.com>
2018-09-14 13:57:25 +1200

    bullet working with collision

--- bullet.js
+++ bullet.js
@@ -11,20 +11,49 @@ class bullet{
             canvasContext.fillStyle = this.c;
             canvasContext.fillRect(this.x, this.y, this.w, this.h);
             }
-            move(){
-                this.y -= this.ySpeed;
+        bulletMove(){
+            this.y -= this.ySpeed;
+        }
+     outOfBounds(){
+                return this.y < 0 || this.y > canvas.height || this.x < 0|| this.x > canvas.width;
             }
-            
-}
-function drawMake(){
-           
+            hasHitItem(item){
+                return ((this.x + this.w) >= item.x 
+                        && this.x <= (item.x + item.w))//xcollision
+                
+                        && (this.y >=item.y 
+                        && this.y <=(item.y + item.h));//ycollision
+                       
+            }
+            hasHitEnemy(enemy) {
+                return this.hasHitItem(enemy);
+            }
+            hasCollided(){
+                var self = this;
+                var collided = false;
+                
+                enemies.forEach(function(enemy, i) {
+                    if(self.hasHitEnemy(enemy)) {
+                        delete enemies[i];
+                        collided = true;
+                    }
+                });
+                enemies = enemies.filter(item => item !== undefined);
+                return collided;
+            }
+        }        
+    
+
+function drawBullet(){
+
             const BULLET_WIDTH = 5;
             const BULLET_HEIGHT = 12;
-            var bulletXpos = shipXpos + SHIP_SIZE/2 - BULLET_WIDTH/2;
-            var bulletYpos = shipYpos - BULLET_HEIGHT;;
+            var bulletXpos = ship.x + WIDTH/2 - BULLET_WIDTH/2;
+            var bulletYpos = ship.y - BULLET_HEIGHT;
             var bulletYspeed = 5;
-            
+             
             var b = new bullet(bulletXpos,bulletYpos,BULLET_WIDTH,BULLET_HEIGHT,'white',bulletYspeed);
             
             bullets.push(b);
+
         }
