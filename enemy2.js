39aa0eea3f9663830bed71968870abd3d616aa70
edSBHS <41878755+edSBHS@users.noreply.github.com>
2018-10-05 16:57:30 +1300

    sprite change for enemy

--- enemy2.js
+++ enemy2.js
@@ -19,61 +19,66 @@
         drawingEnemy(){
             canvasContext.drawImage(this.src, this.sx,this.sy,this.sw,this.sh,this.x,this.y,this.w,this.h);
         }
-    
+   
         enemyMove(){
-            this.delay += this.delayRate;
-//            if(this.sw < 100){
-//                this.sw += 50;
-//                console.log(this.sw);
-//               }
             
-           // console.log(this.delay);
+             
+            if(Math.floor(changeDelay) >= 10){
+                this.sx = 0;
+            if(Math.floor(changeDelay) >= 20){
+                changeDelay = 0;
+               }
+            else{
+                this.sx = 43;
+            }
+               } 
+           this.delay += this.delayRate;
             if(Math.floor(this.delay)% 5 == 0 && Math.floor(this.delay) != 0) {
                 this.x += this.xSpeed;
                 this.delay = 0;
-               // console.log('moving');
+                this.sx = 0;
+                changeDelay += 1;
+//                console.log(Math.floor(changeDelay));
+                
             }
-            
+
             
         }
+
         enemyDrop(){
             if(this.x > canvas.width - this.w || this.x < 0){
-                this.y += (this.h * 2+5) + eGap;
+                this.y += (this.h * 2) + eGap;
                 this.xSpeed *= -1;
                 this.x += this.xSpeed;
-            }
-            
+            } 
         }
-  
-
     }
-
+        
     function drawMakeEnemies() {
   
         var eWidth = 35;
         var eHeight = 30;
         var eXpos = enemyCount * (eWidth + eGap);
         var eYpos = eHeight;
-        var sourceX = 48;
-        var sourceY = 31;
+        var sourceW = 48;
+        var sourceH = 31;
+        var sourceY = 0;
+        var sourceX = 0;
+       
+
+        //43 for nxt costume sx
         
         var eYspeed = 1;
-        var eXspeed = 4;
+        var eXspeed = 6;
         var delay = 0;
         var delayRate = 0.1;
       
-        var e2Xpos = enemyCount * (eWidth + eGap) + 1;
-        var e2Ypos = eHeight *2 + 10;
-        var sourceX2 = 100;
-        var sourceY2 = 100;
-        var e2Width = 35;
-        var e2Height = 30;
+
         
-        var e = new Enemy(enemy, 0, 0, sourceX, sourceY, eXpos, eYpos, eWidth, eHeight,eXspeed,delay,delayRate);
-        var e2 = new Enemy(enemy2, 0,0,sourceX2,sourceY2,e2Xpos,e2Ypos,e2Width,e2Height,eXspeed,delay,delayRate);
-        enemies.push(e2);
+   
+ 
+        var e = new Enemy(enemy, sourceX, sourceY ,sourceW, sourceH, eXpos, eYpos, eWidth, eHeight,eXspeed,delay,delayRate);
+
         enemies.push(e);
           enemyCount++;
-
-
     }
